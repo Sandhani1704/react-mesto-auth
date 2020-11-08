e5b5033df818f78e214be9b826c49a87ceeab554
Anna Galkina <sandhani1704@yandex.ru>
2020-11-09 01:07:13 +0300

    Updates

---
+++ precache-manifest.781dad53c251e2c9792ef0d4fb2a6f7b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4364dc88d80e73efe177283e746686d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "654ea0e329aba500eade",
+    "url": "./static/css/main.b1b865a1.chunk.css"
+  },
+  {
+    "revision": "12c42a8a37943c0420d8",
+    "url": "./static/js/2.82cc154f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.82cc154f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654ea0e329aba500eade",
+    "url": "./static/js/main.3032e253.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "./static/media/Close_Icon.c2d0901f.svg"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "./static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "./static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "./static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "./static/media/Vector-like.6c74d9b5.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "./static/media/avatar-button.34c46850.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "./static/media/like-button_active.9fdebb6b.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "./static/media/remove-button.efa6fb04.svg"
+  }
+]);
